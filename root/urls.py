e5de5ffbbb0337054bf69dcb45f0ff2268899acd
dilshod-py <absaitovdev@gmail>
2025-06-03 18:04:25 +0500

    sadf

--- root/urls.py
+++ root/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     # Optional UI:
     path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('api/v1/', include('authenticate.urls')),
+    path('api/v1/auth/', include('authenticate.urls')),
 ]

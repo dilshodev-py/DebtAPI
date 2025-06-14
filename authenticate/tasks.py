326c304527cf863334b87303f5a75a391fe6cd08
dilshod-py <absaitovdev@gmail>
2025-06-14 15:44:09 +0500

    dsafg

--- authenticate/tasks.py
+++ authenticate/tasks.py
@@ -7,18 +7,18 @@ from root.settings import EMAIL_HOST_USER, EMAIL_HOST_PASSWORD, EMAIL_PORT, EMAI
 
 @app.task(bind=True)
 def send_email(self,receiver_email , message_text):
-    sender_email = EMAIL_HOST_USER
-    password = EMAIL_HOST_PASSWORD
+    # sender_email = EMAIL_HOST_USER
+    sender_email = 'absaitovdev@gmail.com'
+    password = 'jyugxbqoubeoiohs'
     message = MIMEMultipart("alternative")
     message["Subject"] = "multipart test"
     message["From"] = sender_email
     message["To"] = receiver_email
-    text = message_text
-
+    text = "salom"
 
     part1 = MIMEText(text, "plain")
     message.attach(part1)
-    with smtplib.SMTP_SSL(EMAIL_HOST, 465) as server:
+    with smtplib.SMTP_SSL(sender_email, EMAIL_PORT) as server:
         server.login(sender_email, password)
         server.sendmail(
             sender_email, receiver_email, message.as_string()

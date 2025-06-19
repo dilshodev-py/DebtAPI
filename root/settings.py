f374ae0c2ad19fbda868328cead33b6faa98993f
dilshod-py <absaitovdev@gmail>
2025-06-19 16:00:39 +0500

    K

--- root/settings.py
+++ root/settings.py
@@ -135,7 +135,7 @@ EMAIL_PORT = 587  # SMTP server port (587 for TLS, 465 for SSL)
 EMAIL_USE_TLS = True  # True for TLS, False for SSL
 EMAIL_HOST_USER = 'absaitovdev@gmail.com'  # SMTP server username
 EMAIL_HOST_PASSWORD = 'fxoreusmeruzitfh'  # SMTP server password
-EMAIL_USE_SSL = False  # Set to True if using SSL
+EMAIL_USE_SSL = True  # Set to True if using SSL
 DEFAULT_FROM_EMAIL = 'absaitovdev@gmail.com'  # Default sender email address
 
 
